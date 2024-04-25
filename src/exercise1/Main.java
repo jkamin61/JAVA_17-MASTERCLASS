27bd284b8470512af347a31fd9f1dbe87b081b65
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2024-04-25 14:11:11 +0200

    Task_01 - Positive, Negative or Zero

---
+++ src/exercise1/Main.java
@@ -0,0 +1,9 @@
+package exercise1;
+
+public class Main {
+    public static void main(String[] args) {
+        PositiveNegativeZero.checkNumber(5);
+        PositiveNegativeZero.checkNumber(-5);
+        PositiveNegativeZero.checkNumber(0);
+    }
+}
