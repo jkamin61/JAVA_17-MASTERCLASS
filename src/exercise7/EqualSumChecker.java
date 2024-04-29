5d405170274e2b63cf5a60eec8c79e304967b31c
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2024-04-29 18:57:37 +0200

    Task_07 - Equal sum checker

---
+++ src/exercise7/EqualSumChecker.java
@@ -0,0 +1,7 @@
+package exercise7;
+
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int value1, int value2, int value3) {
+        return (value1 + value2) == value3;
+    }
+}
