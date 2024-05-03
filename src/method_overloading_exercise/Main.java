5d5ab3a0ff6e8e21d4461e8050811c457240762e
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2024-05-03 11:30:09 +0200

    method overloading exercise

---
+++ src/method_overloading_exercise/Main.java
@@ -0,0 +1,21 @@
+package method_overloading_exercise;
+
+public class Main {
+    public static void main(String[] args) {
+        convertToCentimeters(6);
+        convertToCentimeters(6, 11);
+    }
+
+    public static double convertToCentimeters(int feet) {
+        System.out.println(feet + " feet equals: " + ((feet * 12) * 2.54) + " centimeters");
+        return (feet * 12) * 2.54;
+    }
+
+    public static double convertToCentimeters(int feet, int inches) {
+        double parsedFeetToCentimeters = (feet * 12) * 2.54;
+        double parsedInchesToCentimeters = inches * 2.54;
+        System.out.println(feet + " feet and " + inches + " inches equals: " + ((parsedFeetToCentimeters + parsedInchesToCentimeters)) + " centimeters");
+        return parsedFeetToCentimeters + parsedInchesToCentimeters;
+    }
+}
+
