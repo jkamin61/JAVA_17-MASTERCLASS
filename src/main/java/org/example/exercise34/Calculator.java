0eaa44c68094da5edf7fb39a2db2b81b19c84fa6
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2024-09-22 21:45:40 +0200

    carpet calculator

---
+++ src/main/java/org/example/exercise34/Calculator.java
@@ -0,0 +1,15 @@
+package org.example.exercise34;
+
+public class Calculator {
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost() {
+        return floor.getArea() * carpet.getCost();
+    }
+}
