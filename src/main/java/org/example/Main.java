778fb5be2a9b514843b83643ee1347a4a26a7f20
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2024-09-22 22:17:29 +0200

    my favorite algo - bubble sort =]

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr1 = {21, 41, 121, 56, 129, 43};
+        System.out.println(Arrays.toString(bubbleSort(arr1)));
     }
+
+    public static int[] bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        do {
+            swapped = false;
+            for (int i = 1; i < n; i++) {
+                if (arr[i - 1] > arr[i]) {
+                    int temp = arr[i - 1];
+                    arr[i - 1] = arr[i];
+                    arr[i] = temp;
+                    swapped = true;
+                }
+            }
+            n--;
+        } while (swapped);
+        return arr;
+    }
+
+
 }
