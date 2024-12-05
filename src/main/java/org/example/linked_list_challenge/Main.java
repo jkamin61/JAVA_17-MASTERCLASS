7f0966ab0bd1fb610ef6663cab75f5a526e8f422
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2024-12-05 18:29:33 +0100

    linked list challenge

--- src/main/java/org/example/linked_list_challenge/Main.java
+++ src/main/java/org/example/linked_list_challenge/Main.java
@@ -3,9 +3,6 @@ package org.example.linked_list_challenge;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-record Place(String name, int distance) {
-}
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
---
+++ src/main/java/org/example/linked_list_challenge/Place.java
@@ -0,0 +1,4 @@
+package org.example.linked_list_challenge;
+
+public record Place(String name, int distance) {
+}
